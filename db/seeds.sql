8e5d62a01a8b7442cd67340ca07d7e8e934c908e
Steven Stot <sstott09@yahoo.com>
2020-02-06 16:36:10 -0700

    Revised layout & Styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ("Albuquerque Turkey, Hatch Green Chile, and Asadero Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES (" - Albuquerque Turkey, Hatch Green Chile, and Asadero Cheese Burger");
 
-INSERT INTO burgers (burger_name) VALUES ("The Bosque Double Cheese Bourbon Glazed Onion Western Beyond Burger");
+INSERT INTO burgers (burger_name) VALUES (" - The Bosque Double Cheese Bourbon Glazed Onion Western Beyond Burger");
 
-INSERT INTO burgers (burger_name) VALUES ("Roswell Impossible - An Out of This World Burger");
+INSERT INTO burgers (burger_name) VALUES (" - Roswell Impossible - An Out of This World Burger");
 
-INSERT INTO burgers (burger_name) VALUES ("Santa Fe Special Christmas Style Burger");
+INSERT INTO burgers (burger_name) VALUES (" - Santa Fe Special Christmas Style Burger");
 
-INSERT INTO burgers (burger_name) VALUES ("Taos Tortilla Burger");
+INSERT INTO burgers (burger_name) VALUES (" - Taos Tortilla Burger");
 
-INSERT INTO burgers (burger_name) VALUES ("Famous Jemez Bison Burger");
+INSERT INTO burgers (burger_name) VALUES (" - Famous Jemez Bison Burger");
